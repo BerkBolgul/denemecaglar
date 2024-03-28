346039cacb68892b7fcd2492aab833b1805355e4
Berko <bolgul.berk93@gmail.com>
2024-03-29 00:44:19 +0100

    sum added

--- caglar.cpp
+++ caglar.cpp
@@ -2,6 +2,13 @@ using namespace std;
 
 #include<iostream>
 
+int sum (int a, int b){
+    int c =a+b;
+    return c;
+}
 int main (){
     cout<<"hello"<<endl;
+    int x=sum(3,4);
+    cout<<x<<endl;
+
 }
